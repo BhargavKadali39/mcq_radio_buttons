9e747cf868cc8163a25dc28fb77dfd4043f89a42
Bhargav <bhargavkadali39@gmail.com>
2021-11-13 23:57:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 
+
+function checkButton() {  
+    let chk
+    if(document.getElementById('answer').checked) {
+        chk = 'Correct answer'
+        alert(chk);
+    }else if(document.getElementById('delete4').checked){
+        alert('Wrong, please try again');
+    }else if(document.getElementById('update3').checked){
+        alert('Wrong, please try again');
+    }else if(document.getElementById('read2').checked){
+        alert('Wrong, please try again');
+    }else if(document.getElementById('create1').checked){
+        alert('Wrong, please try again');
+    }
+    else{
+        alert('Please select any option');
+    }
+} 
